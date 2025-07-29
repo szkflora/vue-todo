f95db19aeba2c9feb43c159ec7ae24bcf006ff28
Flora Szallos-Kis <szkisflora@gmail.com>
2025-07-29 13:56:17 +0300

    starting

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="header">
+    <h1 class="title">To do list</h1>
+    <button>+</button>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+</style>
