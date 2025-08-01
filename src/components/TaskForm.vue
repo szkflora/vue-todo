f181374631527c8af10cf23448536cc9889aafef
Flora Szallos-Kis <szkisflora@gmail.com>
2025-08-01 11:07:58 +0300

    task form

---
+++ src/components/TaskForm.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { Task, Importance } from '../types/Task';
+
+const title = ref<string>('')
+const description = ref<string>('')
+const importance = ref<Importance>(Importance.MEDIUM)
+
+let idCounter = 1
+
+function createTask(): Task {
+  const newTask: Task = {
+    id: idCounter++,
+    title: title.value,
+    description: description.value,
+    importance: importance.value,
+    completed: false
+  }
+
+  title.value = ''
+  description.value = ''
+  importance.value = Importance.MEDIUM
+
+  console.log(newTask)
+
+  return newTask
+}
+
+</script>
+
+<template>
+  <input v-model="title" placeholder="Title" />
+  <input v-model="description" placeholder="Description" />
+  <select v-model="importance" placeholder="Importance">
+    <option value="Low"></option>
+  </select>
+</template>
