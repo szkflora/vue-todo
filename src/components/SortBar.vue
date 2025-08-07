9eecf0bafa3091cb26800f92da948fb5c388ba5c
Flora Szallos-Kis <szkisflora@gmail.com>
2025-08-07 12:01:52 +0300

    sort buttons

---
+++ src/components/SortBar.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import BaseButton from './BaseButton.vue';
+</script>
+
+<template>
+    <div class="w-[600px] flex justify-between">
+        <div>
+        <BaseButton>Title</BaseButton>
+        <BaseButton>Description</BaseButton>
+        <BaseButton>Priority</BaseButton>
+        <BaseButton>Date</BaseButton>
+        </div>
+    </div>
+</template>
